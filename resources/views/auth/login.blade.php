9eb732f3bc3f053bfa33c71c21837fe88db14c5c
Jesse <lbarbosa.jessica@gmail.com>
2021-05-19 00:47:01 -0300

    login funcionando
    pagina de login funcional e finalizada

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,12 +5,34 @@
 @section('content')
 <div class="container">
     <div class="lgn-form">
-        <form class="box" action="" method="post">
+        <form class="box" action="{{ route('login') }}" method="POST">
+            @csrf
             <h1>Login</h1>
-            <input type="text" name="" placeholder="Username">
-            <input type="password" name="" placeholder="Password">
-            <input type="submit" name="" value="Login">
-            <a href="#">Forgot your password?</a>
+            <!-- Email Input -->
+            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Email">
+            @error('email')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+
+            <!-- Password Input -->
+            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Password">
+            @error('password')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+
+            <!-- Button Login -->
+            <input type="submit" name="" value="{{ __('Login') }}">
+
+            <!-- Forgot Password Field -->
+            @if (Route::has('password.request'))
+            <a class="btn btn-link" href="{{ route('password.request') }}">
+                {{ __('Forgot Your Password?') }}
+            </a>
+        @endif
         </form>
         
     </div>
